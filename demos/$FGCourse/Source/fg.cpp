4ecad87b5dd80dc7e86dabdc3659f03eeb9663b8
Eero Mutka <eero.mutka@gmail.com>
2024-12-02 16:21:14 +0100

    Drawing box entities (but without half extent)

--- demos/$FGCourse/Source/fg.cpp
+++ demos/$FGCourse/Source/fg.cpp
@@ -41,11 +41,17 @@ struct VertexShader {
 	ID3D11InputLayout* input_layout;
 };
 
+struct Mesh {
+	ID3D11Buffer* vbo;
+	ID3D11Buffer* ibo;
+	int ibo_count;
+};
+
 // -----------------------------------------------------
 
-static ID3D11Buffer* vbo;
-static ID3D11Buffer* ibo;
-static int ibo_count;
+static Mesh mesh_monkey;
+static Mesh mesh_cube;
+
 static ID3D11Buffer* cbo;
 static ID3D11SamplerState* sampler;
 
@@ -137,6 +143,13 @@ static void MaybeRecreateRenderTargets(HT_API* ht, int width, int height) {
 	}
 }
 
+static void UpdateShaderConstants(ID3D11DeviceContext* dc, ShaderConstants val) {
+	D3D11_MAPPED_SUBRESOURCE cbo_mapped;
+	dc->Map(cbo, 0, D3D11_MAP_WRITE_DISCARD, 0, &cbo_mapped);
+	*(ShaderConstants*)cbo_mapped.pData = val;
+	dc->Unmap(cbo, 0);
+}
+
 static void Render(HT_API* ht) {
 	if (open_scene) {
 		FGCourse__fg_params_type* params = HT_GetPluginData(FGCourse__fg_params_type, ht);
@@ -169,13 +182,9 @@ static void Render(HT_API* ht) {
 
 		ID3D11DeviceContext* dc = ht->D3D11_device_context;
 
-		// ht->LogInfo("RENDERING");
-
-		D3D11_MAPPED_SUBRESOURCE cbo_mapped;
-		dc->Map(cbo, 0, D3D11_MAP_WRITE_DISCARD, 0, &cbo_mapped);
-		ShaderConstants* constants = (ShaderConstants*)cbo_mapped.pData;
-		constants->ws_to_cs = ws_to_cs;
-		dc->Unmap(cbo, 0);
+		ShaderConstants constants = {};
+		constants.ws_to_cs = ws_to_cs;
+		UpdateShaderConstants(dc, constants);
 
 		dc->ClearDepthStencilView(depth_target_view, D3D11_CLEAR_DEPTH, 1.0f, 0);
 		
@@ -200,19 +209,51 @@ static void Render(HT_API* ht) {
 
 		///////////////////////////////////////////////////////////////////////////////////////////
 
-		u32 vbo_stride = sizeof(Vertex);
-		u32 vbo_offset = 0;
-		dc->IASetVertexBuffers(0, 1, &vbo, &vbo_stride, &vbo_offset);
-		dc->IASetIndexBuffer(ibo, DXGI_FORMAT_R32_UINT, 0);
-
-		// dc->PSSetShaderResources(0, 1, &texture_srv);
-
 		float clear_color[] = {0.2f, 0.4f, 0.8f, 1.f};
 		dc->ClearRenderTargetView(color_target_view, clear_color);
 		dc->OMSetRenderTargets(1, &color_target_view, depth_target_view);
 
-		dc->DrawIndexed(ibo_count, 0, 0);
+		u32 vbo_stride = sizeof(Vertex);
 		
+		{
+			u32 vbo_offset = 0;
+			dc->IASetVertexBuffers(0, 1, &mesh_monkey.vbo, &vbo_stride, &vbo_offset);
+			dc->IASetIndexBuffer(mesh_monkey.ibo, DXGI_FORMAT_R32_UINT, 0);
+			dc->DrawIndexed(mesh_monkey.ibo_count, 0, 0);
+		}
+		
+		for (HT_ItemGroupEach(&open_scene->entities, i)) {
+			Scene__SceneEntity* entity = HT_GetItem(Scene__SceneEntity, &open_scene->entities, i);
+
+			// draw entity as cube
+			//Scene__BoxComponent
+			
+			mat4 ls_to_ws =
+				M_MatScale(entity->scale) *
+				M_MatRotateX(entity->rotation.x * M_DegToRad) *
+				M_MatRotateY(entity->rotation.y * M_DegToRad) *
+				M_MatRotateZ(entity->rotation.z * M_DegToRad) *
+				M_MatTranslate(entity->position);
+
+			constants.ws_to_cs = ls_to_ws * ws_to_cs;
+			UpdateShaderConstants(dc, constants);
+
+			{
+				u32 vbo_offset = 0;
+				dc->IASetVertexBuffers(0, 1, &mesh_cube.vbo, &vbo_stride, &vbo_offset);
+				dc->IASetIndexBuffer(mesh_cube.ibo, DXGI_FORMAT_R32_UINT, 0);
+				dc->DrawIndexed(mesh_cube.ibo_count, 0, 0);
+			}
+
+			if (InputIsDown(ht->input_frame, HT_InputKey_Y)) {
+				entity->position.x += 0.1f;
+			}
+
+			if (InputIsDown(ht->input_frame, HT_InputKey_T)) {
+				entity->position.x -= 0.1f;
+			}
+		}
+
 		{
 			D3D11_VIEWPORT present_viewport = {};
 			present_viewport.TopLeftX = (float)open_scene_rect_min.x;
@@ -297,15 +338,11 @@ static ID3D11PixelShader* LoadPixelShader(HT_API* ht, string shader_path) {
 	return shader;
 }
 
-HT_EXPORT void HT_LoadPlugin(HT_API* ht) {
-	FGCourse__fg_params_type* params = HT_GetPluginData(FGCourse__fg_params_type, ht);
-	HT_ASSERT(params);
-
-	// obj loading
+static Mesh LoadOBJ(HT_API* ht, const char* file_cstr) {
 
 	// TODO: fix working directory
 	// std::ifstream file("C:/dev/Hatch/test_assets/cube.obj");
-	std::ifstream file("C:/dev/Hatch/test_assets/monkey.obj");
+	std::ifstream file(file_cstr);
 
 	std::vector<vec3> verts;
 	std::vector<int> indices;
@@ -353,6 +390,21 @@ HT_EXPORT void HT_LoadPlugin(HT_API* ht) {
 		file.close();
 	}
 
+	Mesh mesh = {};
+	mesh.vbo = CreateGPUBuffer(ht, (int)verts.size() * sizeof(Vertex), D3D11_BIND_VERTEX_BUFFER, verts.data());
+	mesh.ibo = CreateGPUBuffer(ht, (int)indices.size() * sizeof(u32), D3D11_BIND_INDEX_BUFFER, indices.data());
+	mesh.ibo_count = (int)indices.size();
+	return mesh;
+}
+
+HT_EXPORT void HT_LoadPlugin(HT_API* ht) {
+	FGCourse__fg_params_type* params = HT_GetPluginData(FGCourse__fg_params_type, ht);
+	HT_ASSERT(params);
+
+	// obj loading
+	mesh_monkey = LoadOBJ(ht, "C:/dev/Hatch/test_assets/monkey.obj");
+	mesh_cube = LoadOBJ(ht, "C:/dev/Hatch/test_assets/cube.obj");
+	
 	// create cbo
 	D3D11_BUFFER_DESC cbo_desc = {};
 	cbo_desc.ByteWidth      = sizeof(ShaderConstants) + 0xf & 0xfffffff0; // ensure constant buffer size is multiple of 16 bytes
@@ -369,10 +421,6 @@ HT_EXPORT void HT_LoadPlugin(HT_API* ht) {
 	sampler_desc.AddressW       = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampler_desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 	ht->D3D11_device->CreateSamplerState(&sampler_desc, &sampler);
-
-	vbo = CreateGPUBuffer(ht, (int)verts.size() * sizeof(Vertex), D3D11_BIND_VERTEX_BUFFER, verts.data());
-	ibo = CreateGPUBuffer(ht, (int)indices.size() * sizeof(u32), D3D11_BIND_INDEX_BUFFER, indices.data());
-	ibo_count = (int)indices.size();
 
 	string main_shader_path = "C:/dev/Hatch/demos/$FGCourse/Shaders/main_shader.hlsl";
 	string present_shader_path = "C:/dev/Hatch/demos/$FGCourse/Shaders/present_shader.hlsl";
